d5c7f297c3197717a8b6430880d680fdfa4db99a
YashKiranPardeshi <yashpardeshi12300@gmail.com>
2022-04-14 17:19:46 +0530

    MODIFIED: Checks for a Option and Winning Position 100

--- src/com/infogalaxy/snake/SnakeAndLadderSimulator.java
+++ src/com/infogalaxy/snake/SnakeAndLadderSimulator.java
@@ -1,26 +1,52 @@
 package com.infogalaxy.snake;
-
 import java.util.Random;
-
 public class SnakeAndLadderSimulator {
-    //UC-1-single play position
-    public void playerposition() {
-        System.out.println("** Welcome To Snake And Ladder Game **");
-        int playerposition = 0;
-        System.out.println(" Player Position : " + playerposition);
+    //UC-1-Position of player
+  int position = 0;
+  //Define Constant For Options
+    final int NO_PLAY = 1;
+    final int LADDER = 2;
+    final int SNAKE = 3;
+  // Showing The player current position
+    public void showPosition(){
+        System.out.println("Player position :" +position);
     }
-    //UC-2-use Random Number  Get A NUmber Between 1 TO 6
-    public void rollsDie() {
-        Random random = new Random();
-        int rollsDie = (int) ((Math.random()*(6-1)))+1;
-        System.out.println("Rolls Die Number IS : " + rollsDie);
+    //-2-Rolling The Die To Position
+    public void rollDie() {
+        while (position != 100) {
+            System.out.println("_________________________________");
+            Random random = new Random();
+            int dieNo = (int) ((Math.random() * (7 - 1)) + 1);
+            System.out.println("DieNO :" + dieNo);
+            //uc-3- Checking for option to play
+            int option = (int) ((Math.random() * (4 - 1)) + 1);
+            System.out.println("Option No :" + option);
+            switch (option) {
+                case NO_PLAY:
+                    System.out.println("NO PLAY Arrived. stay on same position");
+                    break;
+                case LADDER:
+                    System.out.println("HURRAY !!! YOU get LADDER");
+                    position = position + dieNo;
+                    break;
+                case SNAKE:
+                    System.out.println("OOP !!! YOU Get Snake");
+                    position = position - dieNo;
+                    break;
+            }
+            System.out.println("Position:"+position);
+        }
     }
-    public static void main(String[] args) {
-        SnakeAndLadderSimulator snake = new SnakeAndLadderSimulator();
-        snake.playerposition();
-        snake.rollsDie();
+    public static void main (String[] args){
+        //Creating player1 object
+        SnakeAndLadderSimulator player1 = new SnakeAndLadderSimulator();
+        // showing player1 position
+         player1.showPosition();
+         //Rolling The Die
+        player1.rollDie();
+        //Rolling The Die
+        player1.showPosition();
     }
-
 }
 
 
---
+++ src/com/infogalaxy/snake/constants.java
@@ -0,0 +1,7 @@
+package com.infogalaxy.snake;
+
+public class constants {
+    public static final int NO_PLAY = 1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
+}
